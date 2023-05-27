4d77248fcdd9ac84aad132b6d0f85383442e3d0d
qweasd602 <53993828+qweasd602@users.noreply.github.com>
2023-05-26 19:04:30 -0700

    Add files via upload
    COMPLETE REVAMP

--- script.js
+++ script.js
@@ -1,3 +1,42 @@
-function playpause() {
-    
+
+// PLAY/PAUSE RAIN BACKGROUND BUTTON
+// var video = document.getElementById("rainbackground");
+
+// var btn = document.getElementById("myBtn");
+
+// function PPbackground()
+// {
+//   if (video.paused)
+//   {
+//     video.play();
+//     btn.innerHTML = "<img src = \"icons/pause.png\" width = \"40px\" height= \"40px\">";
+//   }
+//   else 
+//   {
+//     video.pause();
+//     btn.innerHTML = "<img src=\"icons/play.png\" width=\"40px\" height=\"40x\">";
+//   }
+// }
+
+// PLAY/PAUSE SONG BUTTON
+var btn2 = document.getElementById("myBtn2");
+var music = document.getElementById("musicplayer");
+
+function MusicPlay()
+{
+  if (music.paused)
+  {
+    music.play();
+    btn2.innerHTML = "<img src = \"icons/pause.png\" width = \"40px\" height= \"40px\" top = \"5px\" >";
+  }
+  else
+  {
+    music.pause();
+    btn2.innerHTML = "<img src = \"icons/play.png\" width = \"40px\" height= \"40px\" top = \"5px\" >";
+  }
 }
+
+// function ProjectsJump()
+// {
+//     if ()
+// }
